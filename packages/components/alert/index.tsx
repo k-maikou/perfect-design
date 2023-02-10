4b0e754e56a3c21d8bd7e9ca7a93fff414ab0b48
康健斌 <kkkkkk@kangjianbindeMacBook-Pro.local>
2023-02-10 16:09:31 +0800

    feat: 第一个组件demo

---
+++ packages/components/alert/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export interface AlertProps {
+	kind?: 'info' | 'positive' | 'negative' | 'warning'
+	children?: any
+}
+
+export type KindMap = Record<Required<AlertProps>['kind'], string>
+
+const prefixCls = 'happy-alert'
+
+const kinds: KindMap = {
+	info: '#5352ED',
+	positive: '#2ED573',
+	negative: '#FF4757',
+	warning: '#FFA502'
+}
+
+const Alert: React.FC<AlertProps> = ({ children, kind = 'info', ...rest }) => (
+	<div
+		className={prefixCls}
+		style={{
+			background: kinds[kind]
+		}}
+		{...rest}
+	>
+		{children}
+	</div>
+)
+
+export default Alert
