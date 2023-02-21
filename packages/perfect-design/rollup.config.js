ea4c5fbed8e73719957c90c8ca0e7e0dac6f8486
康健斌 <kkkkkk@kangjianbindeMacBook-Pro.local>
2023-02-21 11:59:43 +0800

    feat: rollup打包

--- packages/perfect-design/rollup.config.js
+++ packages/perfect-design/rollup.config.js
@@ -1,3 +1,13 @@
+import typescript from 'rollup-plugin-typescript2'
+import less from 'rollup-plugin-less'
+import clear from 'rollup-plugin-clear'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+import { uglify } from 'rollup-plugin-uglify'
+import copy from 'rollup-plugin-copy'
+
 export default {
 	input: ['./src/index.ts'],
 	output: [
@@ -16,6 +26,36 @@ export default {
 			format: 'es',
 			name: 'index.js'
 		}
+	],
+	plugins: [
+		typescript(), // 会自动读取 文件tsconfig.json配置
+		less({ output: './dist/style/index.css' }),
+		clear({
+			targets: ['dist']
+		}),
+		resolve(),
+		commonjs({
+			ignoreGlobal: true,
+			include: /\/node_modules\//,
+			external: ['react', 'react-dom', 'styled-components'],
+			namedExports: {
+				react: Object.keys(require('react'))
+			}
+		}),
+		babel({
+			exclude: 'node_modules/**',
+			runtimeHelpers: true
+		}),
+		terser(),
+		uglify(),
+		copy({
+			targets: [
+				{
+					src: '../../scripts/globalStyle/compiled-colors.less',
+					dest: 'distt/style'
+				}
+			]
+		})
 	],
 	external: ['react', 'react-dom']
 }
